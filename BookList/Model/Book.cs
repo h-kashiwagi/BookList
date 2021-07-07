368689da2d0f39839db4617137bae06c2550e5a1
h-kashiwagi <h-kashiwagi@fpcom.jp>
2021-07-07 16:29:09 +0900

    create BookListProject

---
+++ BookList/Model/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations; //[Key]を使うためモジュール
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookList.Model
+{
+    public class Book
+    {
+        [Key] //プライマリーキー（主キー）
+        public int Id { get; set; }
+
+        [Required] //nullはNG
+        public string Name { get; set; } //Bookの名前
+        public string Author { get; set; } //Bookの著者
+    }
+}
